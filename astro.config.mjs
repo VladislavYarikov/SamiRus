6d1fc74d1d13709dc02712851f2149a082d4706c
tapeduck <157802614+tapeduck@users.noreply.github.com>
2024-11-21 06:30:26 +0300

    feat: add new redirect rules for /know-more and /contact-us to WhatsApp

--- astro.config.mjs
+++ astro.config.mjs
@@ -47,6 +47,14 @@ export default defineConfig({
     '/privacy': {
       status: 301,
       destination: '/legal/privacy-policy.pdf'
-    }
+    },
+    '/know-more': {
+      status: 302,
+      destination: 'https://wa.me/79300130949?text=Привет.%20Хочу%20получать%20заказы%20раньше!%20'
+    },
+    '/contact-us': {
+      status: 302,
+      destination: 'https://wa.me/79300130949?text=%D0%97%D0%B4%D1%80%D0%B0%D0%B2%D1%81%D1%82%D0%B2%D1%83%D0%B9%D1%82%D0%B5%2C+%D0%B5%D1%81%D1%82%D1%8C+%D0%B2%D0%BE%D0%BF%D1%80%D0%BE%D1%81+%D0%BF%D0%BE+%D0%BF%D1%80%D0%B8%D0%BB%D0%BE%D0%B6%D0%B5%D0%BD%D0%B8%D1%8E+%D0%A1%D0%B0%D0%BC%D0%B8%3A+'
+    },
   }, 
 });
