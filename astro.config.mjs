b767639ec06ff897a01fc6fbc7965bc1fc987e3d
tapeduck <157802614+tapeduck@users.noreply.github.com>
2025-02-22 12:40:18 +0300

    fix: update redirect destinations for /know-more and /contact-us to include proper WhatsApp links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,28 @@
 // @ts-check
-import svelte from '@astrojs/svelte';
-import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
-import { defineConfig } from 'astro/config';
+import svelte from "@astrojs/svelte";
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from "astro/config";
 
-import node from '@astrojs/node';
+import node from "@astrojs/node";
 import { loadEnv } from "vite";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
-const isVercel = !!(process.env.VERCEL);
+const isVercel = !!process.env.VERCEL;
 
 const loadEnvConfig = () => {
   // @ts-expect-error
   let { PUBLIC_DOMAIN } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
-  if (process.env.NODE_ENV === 'development') {
-    PUBLIC_DOMAIN = 'http://localhost:4321'
+  if (process.env.NODE_ENV === "development") {
+    PUBLIC_DOMAIN = "http://localhost:4321";
   } else if (!PUBLIC_DOMAIN) {
-    throw new Error('PUBLIC_DOMAIN is required');
+    throw new Error("PUBLIC_DOMAIN is required");
   }
 
   return { PUBLIC_DOMAIN };
-}
+};
 
 const { PUBLIC_DOMAIN } = loadEnvConfig();
 
@@ -31,30 +31,38 @@ export default defineConfig({
   output: "server",
   site: PUBLIC_DOMAIN,
 
-  adapter: isVercel? vercel() : node({
-    mode: 'standalone',
-  }),
+  adapter: isVercel
+    ? vercel()
+    : node({
+        mode: "standalone",
+      }),
 
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), svelte(), sitemap()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    svelte(),
+    sitemap(),
+  ],
 
   redirects: {
-    '/terms': {
+    "/terms": {
       status: 301,
-      destination: '/legal/user-agreement.pdf'
+      destination: "/legal/user-agreement.pdf",
     },
-    '/privacy': {
+    "/privacy": {
       status: 301,
-      destination: '/legal/privacy-policy.pdf'
+      destination: "/legal/privacy-policy.pdf",
     },
-    '/know-more': {
+    "/know-more": {
       status: 307,
-      destination: '?text=Привет. Хочу получать заказы раньше!'
+      destination:
+        "https://wa.me/79300130949?text=Привет. Хочу получать заказы раньше!",
     },
-    '/contact-us': {
+    "/contact-us": {
       status: 307,
-      destination: 'https://wa.me/79300130949?text=Здравствуйте, есть вопрос по приложению Сами: '
+      destination:
+        "https://wa.me/79300130949?text=Здравствуйте, есть вопрос по приложению Сами: ",
     },
-  }, 
+  },
 });
